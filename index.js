c4cbfb2325e277a442b202ecb0a99a6beef4a7eb
Daniel Lasche <dlasche@gmail.com>
2021-05-19 15:44:59 -0700

    changes to PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const { uid } = require("./services.js");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
